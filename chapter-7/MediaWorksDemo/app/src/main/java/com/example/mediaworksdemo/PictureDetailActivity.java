7f7956bd22734f1e20905d9fa64d8b4f001a319d
wonderjeo <455708864@qq.com>
2021-07-15 22:25:18 +0800

    添加课件

---
+++ chapter-7/MediaWorksDemo/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.mediaworksdemo;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    String mockUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Android_logo_2019_%28stacked%29.svg/400px-Android_logo_2019_%28stacked%29.svg.png";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+
+        ImageView imageView = findViewById(R.id.iv_detail);
+        Glide.with(this).load(mockUrl).into(imageView);
+    }
+}
