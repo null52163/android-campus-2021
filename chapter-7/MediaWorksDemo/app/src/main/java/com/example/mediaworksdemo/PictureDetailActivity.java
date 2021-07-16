e5257de4b2fc7ce56bba3d0f908128f23e7b6d69
Dezhao <wangdezhao@bytedance.com>
2021-07-16 18:07:49 +0800

    修改demo

--- chapter-7/MediaWorksDemo/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
+++ chapter-7/MediaWorksDemo/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.mediaworksdemo;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -11,12 +12,29 @@ import com.bumptech.glide.Glide;
 public class PictureDetailActivity extends AppCompatActivity {
 
     String mockUrl = "https://lf1-cdn-tos.bytescm.com/obj/static/ies/bytedance_official_cn/_next/static/images/0-390b5def140dc370854c98b8e82ad394.png";
+    String mockErrorUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Android_logo_2019_%28stacked%29.svg/400px-Android_logo_2019_%28stacked%29.svg.png";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
 
         ImageView imageView = findViewById(R.id.iv_detail);
-        Glide.with(this).load(mockUrl).into(imageView);
+        Button btnSuccess = findViewById(R.id.btn_load_success);
+        Button btnFail = findViewById(R.id.btn_load_fail);
+
+        btnSuccess.setOnClickListener( v -> {
+            Glide.with(this).load(mockUrl)
+                    .placeholder(R.drawable.loading_green)
+                    .error(R.drawable.error_red)
+                    .into(imageView);
+        });
+
+        btnFail.setOnClickListener( v -> {
+            Glide.with(this).load(mockErrorUrl)
+                    .placeholder(R.drawable.loading_green)
+                    .error(R.drawable.error_red)
+                    .into(imageView);
+        });
     }
 }
