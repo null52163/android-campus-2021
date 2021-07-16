c29a76d271f80dacb7dfe7ea9cb8121cf8c3eb14
Dezhao <wangdezhao@bytedance.com>
2021-07-16 15:03:50 +0800

    修改demo

--- chapter-7/MediaWorksDemo/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
+++ chapter-7/MediaWorksDemo/app/src/main/java/com/example/mediaworksdemo/PictureDetailActivity.java
@@ -10,7 +10,7 @@ import com.bumptech.glide.Glide;
 
 public class PictureDetailActivity extends AppCompatActivity {
 
-    String mockUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Android_logo_2019_%28stacked%29.svg/400px-Android_logo_2019_%28stacked%29.svg.png";
+    String mockUrl = "https://lf1-cdn-tos.bytescm.com/obj/static/ies/bytedance_official_cn/_next/static/images/0-390b5def140dc370854c98b8e82ad394.png";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
